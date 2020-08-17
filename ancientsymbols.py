1d2e780b7dd9bab1d4dbb4281d0be61acad89430
Christoph <christoph.pranzl@pranzl.net>
2020-08-17 11:34:09 +0200

    First draft

---
+++ ancientsymbols.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
+
+__author__ = "Christoph Pranzl"
+__version__ = "0.1.0"
+__license__ = "GNU GPLv3"
+
+import argparse
+import random
+from logzero import logger
+
+GREEN = ['1','2','3','Terror','Peril','Lore']
+YELLOW = ['1','2','3','4','Peril','Lore']
+RED = ['2','3','Peril','Lore','Joker']
+
+def main(args):
+    """ Main entry point of the app """
+    logger.info(random.sample(GREEN, 1))
+    logger.info(args)
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    parser = argparse.ArgumentParser()
+
+    # Required positional argument
+    parser.add_argument("arg", help="Required positional argument")
+
+    # Optional argument flag which defaults to False
+    parser.add_argument("-f", "--flag", action="store_true", default=False)
+
+    # Optional argument which requires a parameter (eg. -d test)
+    parser.add_argument("-n", "--name", action="store", dest="name")
+
+    # Optional verbosity counter (eg. -v, -vv, -vvv, etc.)
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="Verbosity (-v, -vv, etc)")
+
+    # Specify output of "--version"
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="%(prog)s (version {version})".format(version=__version__))
+
+    args = parser.parse_args()
+    main(args)
